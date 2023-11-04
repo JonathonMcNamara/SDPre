32f1efc4f3f2a8df736d93b9746c49ceaddca524
JonathonMcNamara <jonathonmcnamara1234@hotmail.com>
2023-11-04 15:57:04 -0600

    Objective2Lab3 complete

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int year_born = 1995;
+    String month = "February";
+    boolean todayIsAWeekend = true;
+    System.out.println(year_born);
+    System.out.println(month);
+    System.out.println(todayIsAWeekend);
+  }
+}
